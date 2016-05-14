47b1a3b9f5b24eb527b63816bdeb64844c0bf0c7
HaraldNordgren <haraldnordgren@gmail.com>
2016-05-15 01:23:58 +0200

    Changed postgres script to connect to heroku database

--- sql/scripts/postgres.sh
+++ sql/scripts/postgres.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-export PGPASSWORD=admin
-psql -U postgres -h localhost -d odds_data
+#export PGPASSWORD=admin
+#psql -U postgres -h localhost -d odds_data
+
+export PGPASSWORD='tf5q17_nrEOswfolbd3PS6wmNF'
+psql -U 'pjormmuewnnkwm' -h 'ec2-54-83-198-111.compute-1.amazonaws.com' -d 'da3j6ejvc5arr0' -p 5432
