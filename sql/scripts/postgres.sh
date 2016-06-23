25c59e1c6a63fd866146d6a99bcc304d1875c8b9
HaraldNordgren <haraldnordgren@gmail.com>
2016-06-23 23:15:19 +0200

    Updated sql script for heroku database

--- sql/scripts/postgres.sh
+++ sql/scripts/postgres.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-export PGPASSWORD=admin
-psql -U postgres -h localhost -d odds_data
+#self.connection = psycopg2.connect(host='ec2-54-83-198-111.compute-1.amazonaws.com', user='pjormmuewnnkwm', password='tf5q17_nrEOswfolbd3PS6wmNF', dbname='da3j6ejvc5arr0', port=5432)
+
+export PGPASSWORD='tf5q17_nrEOswfolbd3PS6wmNF'
+psql -U 'pjormmuewnnkwm' -h 'ec2-54-83-198-111.compute-1.amazonaws.com' -d 'da3j6ejvc5arr0' -p 5432
