2cfe02c8edcf14ddb6881ed9380eb78005952375
Harald Nordgren <haraldnordgren@gmail.com>
2018-08-22 00:23:27 +0200

    improve run_once.sh

--- run_once.sh
+++ run_once.sh
@@ -1,18 +1,36 @@
 #!/bin/bash
 
 set -e
+PYTHON=python3
+
+function require {
+    cmd=$1
+    command -v "$cmd" > /dev/null 2>&1 || {
+        echo >&2 "I require '$cmd' but it's not installed. Aborting."
+        exit 1
+    }
+}
+require scrapy
+require $PYTHON
+
+function python_require {
+    package=$1
+    if ! $PYTHON -m pip show "$package" 1> /dev/null; then
+        echo >&2 "I require python package '$package' but it's not installed."
+        exit 1
+    fi
+}
+python_require scrapy
 
 if [[ -n "$DYNO" ]]; then
     echo "On Heroku"
 fi
-
 cd src
-PYTHON=python3
 
 scrapy runspider betway_scrapy.py 
-$PYTHON scrape_test.py
+#$PYTHON scrape_test.py
 
 printf "\nCALCULATING ARBITRAGES\n"
 $PYTHON database_test.py
-
 printf "\n"
+
